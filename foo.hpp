e9a9758a9b4abb954a25663afba587682720622a
Wiktor Łagoszyn <wiktorlag2003@wp.pl>
2025-11-29 14:43:23 +0100

    29.11

--- foo.hpp
+++ foo.hpp
@@ -7,6 +7,16 @@
 
 std::vector< char > foo(std::list< Human >& people)
 {
-    // Twoja implementacja tutaj
-    return {};
+    std::vector< char > result;
+
+ for (auto osoba = people.rbegin(); osoba != people.rend(); ++osoba)
+ {
+     osoba->birthday();
+     
+     if (osoba->isMonster())
+         result.push_back('n');
+     else
+         result.push_back('y');
+ }
+    return result;
 }
